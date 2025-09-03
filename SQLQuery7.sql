a0464e4b6d8d7d3850c2c56e9e6a0247b6b4169c
Shivam Dharpure <shivam1234pawar@gmail.com>
2025-09-03 16:13:30 +0530

    Delete SQLQuery7.sql

--- SQLQuery7.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW vw_ChurnData as
-	select * from prod_Churn where Customer_Status IN ('Churned','Stayed')
-
-CREATE VIEW VW_JoinData as 
-	select * from prod_Churn where Customer_Status = 'Joined'
